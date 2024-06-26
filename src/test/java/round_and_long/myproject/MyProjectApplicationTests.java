68ecf9062a6d4263746db7e0434e3f39490bb621
hyeangjun <popora99@naver.com>
2024-06-26 16:53:20 +0900

    기본 세팅

---
+++ src/test/java/round_and_long/myproject/MyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package round_and_long.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
